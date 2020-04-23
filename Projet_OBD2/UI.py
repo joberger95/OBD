4b1edb072b4f262f4abc4a81b5130882e34393a3
joberger95 <jordan.berger@ynov.com>
2020-04-23 18:35:00 +0200

    start UI

--- Projet_OBD2/UI.py
+++ Projet_OBD2/UI.py
@@ -1,12 +1,20 @@
 from tkinter import *
+import tkinter as tk
 
 
 class Window(Tk):
     def __init__(self):
         super(Window, self).__init__()
+        width = self.winfo_screenwidth()
+        height = self.winfo_screenheight()
         self.title("ODB App")
-        self.minsize(1080, 720)
-        self.iconbitmap("motor.ico")
+        self.config(bg="#c3c0bf")
+        self.geometry("%dx%d" % (width, height))
+
+        self.splash_screen()
+
+    def splash_screen(self):
+        return None
 
 
 window = Window()
